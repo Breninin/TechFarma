0db9596c159fbfbbb5526a782b2f73fb9012c129
andrey <gggamerdosgames@gmail.com>
2023-06-10 07:29:14 -0400

    dvds
    dsdvsdv

--- WpfTechPharma/WpfTechPharma/BancoDados/bd_techpharma.sql
+++ WpfTechPharma/WpfTechPharma/BancoDados/bd_techpharma.sql
@@ -4,7 +4,7 @@ create database bd_techpharma;
 use bd_techpharma;
 
 create table Endereco(
-ende_id int primary key not null,
+ende_id int primary key not null auto_increment,
 ende_estado varchar (200),
 ende_cidade varchar (50),
 ende_bairro varchar (50),
@@ -14,8 +14,19 @@ ende_complemento varchar (200),
 ende_cep varchar (20)
 );
 
+create table Fornecedor (
+forn_id int primary key not null auto_increment,
+forn_razao_social varchar (100),
+forn_nome_fantasia varchar (100),
+forn_email varchar (50),
+forn_contato varchar (20),
+forn_inscricao_estudal varchar (50),
+fk_ende_id int,
+foreign key (fk_ende_id) references Endereco (ende_id)
+);
+
 create table Cliente (
-clie_id int primary key not null,
+clie_id int primary key not null auto_increment,
 clie_nome varchar (50),
 clie_sexo varchar (20),
 clie_nascimento date,
@@ -23,12 +34,12 @@ clie_rg varchar (25),
 clie_cpf varchar (25),
 clie_email varchar (50),
 clie_contato varchar (20),
-id_ende_fk int,
-foreign key (id_ende_fk) references Endereco (ende_id)
+fk_ende_id int,
+foreign key (fk_ende_id) references Endereco (ende_id)
 );
 
 create table Funcionario (
-func_id int primary key not null,
+func_id int primary key not null auto_increment,
 func_nome varchar (50),
 func_sexo varchar (20),
 func_nascimento date,
@@ -38,92 +49,20 @@ func_email varchar (50),
 func_contato varchar (20),
 func_funcao varchar(50),
 func_salario float,
-id_ende_fk int,
-foreign key (id_ende_fk) references Endereco (ende_id)
+fk_ende_id int,
+foreign key (fk_ende_id) references Endereco (ende_id)
 );
 
 create table Usuario (
-usua_id int primary key not null,
+usua_id int primary key not null auto_increment,
 usua_login varchar (20),
 usua_senha varchar (20),
-id_func_fk int,
-foreign key (id_func_fk) references Funcionario (func_id)
-);
-
-create table Caixa (
-caix_id int primary key not null,
-caix_numero int,
-caix_data date,
-caix_horario_inicial time,
-caix_horario_final time,
-caix_status varchar (20),
-caix_saldo_inicial float,
-caix_saldo_final float,
-caix_total_entrada float,
-caix_total_saida float
-);
-
-create table Despesa (
-desp_id int primary key not null,
-desp_data date,
-desp_valor float,
-desp_desc varchar (100),
-desp_tipo varchar (20),
-desp_quantidade_parcelas int
-);
-
-create table Venda(
-vend_id int primary key not null,
-vend_data date,
-vend_valor float,
-vend_desconto float,
-vend_quantidade_parcelas int,
-id_clie_fk int,
-id_func_fk int,
-foreign key (id_clie_fk) references Cliente (clie_id),
-foreign key (id_func_fk) references Funcionario (func_id)
-);
-
-create table Servico (
-serv_id int primary key not null,
-serv_nome varchar (50),
-serv_valor_venda float,
-serv_duracao time,
-serv_tipo varchar (50)
-);
-
-create table Venda_Servico (
-vaso_id int,
-vaso_quantidade_item int,
-vaso_valor_servico float,
-
-);
-
-create table Fornecedor (
-forn_id int primary key not null,
-forn_razao_social varchar (100),
-forn_nome_fantasia varchar (100),
-forn_email varchar (50),
-forn_contato varchar (20),
-forn_inscricao_estudal varchar (50),
-id_ende_fk int,
-foreign key (id_ende_fk) references Endereco (ende_id)
-);
-
-create table Insumo (
-insu_id int primary key not null,
-insu_nome varchar (50),
-insumo_marca varchar (50),
-insumo_valor_compra float,
-insu_quantidade int,
-insu_tipo varchar (200),
-insu_codigo_barra varchar (50),
-id_forn_fk int,
-foreign key (id_insu_fk) references Fornecedor (forn_id)
+fk_func_id int,
+foreign key (fk_func_id) references Funcionario (func_id)
 );
 
 create table Produto (
-prod_id int primary key not null,
+prod_id int primary key not null auto_increment,
 prod_nome varchar (50),
 prod_marca varchar (50),
 prod_valor_compra float,
@@ -131,12 +70,12 @@ prod_valor_vend float,
 prod_quantidade int,
 prod_tipo varchar (50),
 prod_codigo_barra varchar (50),
-id_forn_fk int,
-foreign key (id_insu_fk) references Fornecedor (forn_id)
+fk_forn_id int,
+foreign key (fk_forn_id) references Fornecedor (forn_id)
 );
 
 create table Medicamento (
-medi_id int primary key not null,
+medi_id int primary key not null auto_increment,
 medi_nome varchar (50),
 medi_marca varchar (20),
 medi_peso_volume varchar (10),
@@ -145,57 +84,187 @@ medi_valor_venda float,
 medi_quantidade int,
 medi_tarja varchar (20),
 medi_codigo_barra varchar (50),
-id_forn_fk int,
-foreign key (id_forn_fk) references Fornecedor (forn_id)
+fk_forn_id int,
+foreign key (fk_forn_id) references Fornecedor (forn_id)
 );
 
-create table Venda_Produto (
-vapo_id int primary key not null,
-vapo_quantidade_item int,
-vapo_valor_item float,
-id_prod_fk int,
-id_vend_fk int,
-foreign key (id_prod_fk) references Produto (prod_id),
-foreign key (id_vend_fk) references Venda (vend_id)
+create table Insumo (
+insu_id int primary key not null auto_increment,
+insu_nome varchar (50),
+insumo_marca varchar (50),
+insumo_valor_compra float,
+insu_quantidade int,
+insu_tipo varchar (200),
+insu_codigo_barra varchar (50),
+fk_forn_id int,
+foreign key (fk_forn_id) references Fornecedor (forn_id)
+);
+
+create table Servico (
+serv_id int primary key not null auto_increment,
+serv_nome varchar (50),
+serv_valor_venda float,
+serv_duracao time,
+serv_tipo varchar (50)
+);
+
+create table Insumo_Servico (
+ioso_id int primary key not null auto_increment,
+ioso_quantidade_insumo int,
+fk_serv_id int,
+fk_insu_id int,
+foreign key (fk_serv_id) references Servico (serv_id),
+foreign key (fk_insu_id) references Insumo (insu_id)
+);
+
+create table Caixa (
+caix_id int primary key not null auto_increment,
+caix_numero int,
+caix_data date,
+caix_horario_inicial time,
+caix_horario_final time,
+caix_status varchar (20),
+caix_saldo_inicial float,
+caix_saldo_final float,
+caix_total_entrada float,
+caix_total_saida float,
+fk_func_id int,
+foreign key (fk_func_id) references Funcionario (func_id)
+);
+
+create table Despesa (
+desp_id int primary key not null auto_increment,
+desp_data date,
+desp_valor float,
+desp_desc varchar (100),
+desp_tipo varchar (20),
+desp_quantidade_parcelas int
 );
 
 create table Compra (
-comp_id int primary key not null,
+comp_id int primary key not null auto_increment,
 comp_data date,
 comp_valor float,
-id_forn_fk int,
-id_desp_fk int,
-foreign key (id_forn_fk) references Fornecedor (forn_id),
-foreign key (id_desp_fk) references Despesa (desp_id)
+fk_forn_id int,
+fk_desp_id int,
+foreign key (fk_forn_id) references Fornecedor (forn_id),
+foreign key (fk_desp_id) references Despesa (desp_id)
 );
 
-create table Compra_Insumo (
-caio_id int,
-caio_quantidade_item int,
-caio_valor_item float,
-id_comp_fk int,
-id_insu_fk int,
-foreign key (id_comp_fk) references Compra (comp_id),
-foreign key (id_insu_fk) references Insumo (insu_id)
+create table Pagamento (
+paga_id int primary key not null auto_increment,
+paga_data date,
+paga_valor float,
+paga_forma_pagamento varchar(50),
+paga_status varchar(20),
+paga_vencimento date,
+paga_numero_parcela int,
+fk_caix_id int,
+fk_desp_id int,
+foreign key (fk_caix_id) references Caixa (caix_id),
+foreign key (fk_desp_id) references Despesa (desp_id)
+);
+
+create table Venda(
+vend_id int primary key not null auto_increment,
+vend_data date,
+vend_valor float,
+vend_desconto float,
+vend_quantidade_parcelas int,
+fk_clie_id int,
+fk_func_id int,
+foreign key (fk_clie_id) references Cliente (clie_id),
+foreign key (fk_func_id) references Funcionario (func_id)
+);
+
+create table Recebimento(
+rece_id int primary key not null auto_increment,
+rece_data date,
+rece_valor float,
+rece_forma_recebimento varchar(50),
+rece_status varchar(20),
+rece_vencimento date,
+rece_numero_parcela int,
+fk_caix_id int,
+fk_vend_id int,
+foreign key (fk_caix_id) references Caixa (caix_id),
+foreign key (fk_vend_id) references Venda (vend_id)
+);
+
+create table Venda_Produto (
+vapo_id int primary key not null auto_increment,
+vapo_quantidade_item int,
+vapo_valor_item float,
+fk_prod_id int,
+fk_vend_id int,
+foreign key (fk_prod_id) references Produto (prod_id),
+foreign key (fk_vend_id) references Venda (vend_id)
+);
+
+create table Venda_Medicamento (
+vamo_id int primary key not null auto_increment,
+vamo_quantidade_item int,
+vamo_valor_item float,
+fk_medi_id int,
+fk_vend_id int,
+foreign key (fk_medi_id) references Medicamento (medi_id),
+foreign key (fk_vend_id) references Venda (vend_id)
+);
+
+create table Venda_Servico (
+vaso_id int primary key not null auto_increment,
+vaso_quantidade_item int,
+vaso_valor_servico float,
+fk_serv_id int,
+fk_vend_id int,
+foreign key (fk_serv_id) references Servico (serv_id),
+foreign key (fk_vend_id) references Venda (vend_id)
 );
 
 create table Compra_Produto (
-capo_id int,
+capo_id int primary key not null auto_increment,
 capo_quantidade_item int,
 capo_valor_item float,
-id_comp_fk int,
-id_prod_fk int,
-foreign key (id_comp_fk) references Compra (comp_id),
-foreign key (id_prod_fk) references Produto (prod_id)
+fk_comp_id int,
+fk_prod_id int,
+foreign key (fk_comp_id) references Compra (comp_id),
+foreign key (fk_prod_id) references Produto (prod_id)
 );
 
 create table Compra_Medicamento (
-camo_id int,
+camo_id int primary key not null auto_increment,
 camo_quantidade_item int,
 campo_valor_item float,
-id_comp_fk int,
-id_medi_fk int,
-foreign key (id_comp_fk) references Compra (comp_id),
-foreign key (id_medi_fk) references Medicamento (medi_id)
+fk_comp_id int,
+fk_medi_id int,
+foreign key (fk_comp_id) references Compra (comp_id),
+foreign key (fk_medi_id) references Medicamento (medi_id)
 );
 
+create table Compra_Insumo (
+caio_id int primary key not null auto_increment,
+caio_quantidade_item int,
+caio_valor_item float,
+fk_comp_id int,
+fk_insu_id int,
+foreign key (fk_comp_id) references Compra (comp_id),
+foreign key (fk_insu_id) references Insumo (insu_id)
+);
+
+create table Prescricao (
+pres_id int primary key not null auto_increment,
+pres_data date,
+pres_patologia varchar(50),
+pres_vendimento date,
+pres_nome_emissor varchar(50),
+pres_clinica_emissora varchar(50),
+pres_scan_documento blob,
+fk_clie_id int,
+fk_medi_id int,
+fk_func_id int,
+fk_vend_id int,
+foreign key (fk_clie_id) references Cliente (clie_id),
+foreign key (fk_medi_id) references Medicamento (medi_id),
+foreign key (fk_func_id) references Funcionario (func_id),
+foreign key (fk_vend_id) references Venda (vend_id)
+);
---
+++ WpfTechPharma/WpfTechPharma/Modelos/ClienteDAO.cs
@@ -0,0 +1,228 @@
+﻿using WpfTechPharma.Auxiliares;
+using WpfTechPharma.BancoDados;
+using WpfTechPharma.Interfaces;
+
+namespace WpfTechPharma.Modelos
+{
+    internal class ClienteDAO : IDAO<Cliente>
+    {
+        private static Conexao conexao;
+
+        public ClienteDAO()
+        {
+            conexao = new Conexao();
+        }
+
+        public void Insert(Cliente t)
+        {
+            try
+            {
+                var query = conexao.Query();
+                query.CommandText =
+                    "insert into " +
+                    "Cliente " +
+                    "(clie_nome, " +
+                    "clie_sexo, " +
+                    "clie_nascimento, " +
+                    "clie_rg, " +
+                    "clie_cpf, " +
+                    "clie_email, " +
+                    "clie_contato, " +
+                    "fk_ende_id) " +
+                    "values " +
+                    "(@nome, " +
+                    "@sexo, " +
+                    "@nascimento, " +
+                    "@rg, " +
+                    "@cpf, " +
+                    "@email, " +
+                    "@contato," +
+                    "@endereco)";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@sexo", t.Sexo);
+                query.Parameters.AddWithValue("@nascimento", t.Nascimento?.ToString("yyyy-MM-dd"));
+                query.Parameters.AddWithValue("@rg", t.RG);
+                query.Parameters.AddWithValue("@cpf", t.CPF);
+                query.Parameters.AddWithValue("@email", t.Email);
+                query.Parameters.AddWithValue("@contato", t.Contato);
+                query.Parameters.AddWithValue("@endereco", t.Endereco);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    throw new Exception("Erro ao salvar o cliente. Verifique o cliente inserido e tente novamente.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Update(Cliente t)
+        {
+            try
+            {
+                var query = conexao.Query();
+                query.CommandText =
+                    "update " +
+                    "Cliente " +
+                    "set " +
+                    "clie_nome = @nome, " +
+                    "clie_sexo = @sexo, " +
+                    "clie_nascimento = @nascimento, " +
+                    "clie_rg = @rg, " +
+                    "clie_cpf = @cpf, " +
+                    "clie_email = @email, " +
+                    "clie_contato = @contato, " +
+                    "fk_ende_id = @endereco " +
+                    "where " +
+                    "(clie_id = @id)";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@sexo", t.Sexo);
+                query.Parameters.AddWithValue("@nascimento", t.Nascimento?.ToString("yyyy-MM-dd"));
+                query.Parameters.AddWithValue("@rg", t.RG);
+                query.Parameters.AddWithValue("@cpf", t.CPF);
+                query.Parameters.AddWithValue("@email", t.Email);
+                query.Parameters.AddWithValue("@contato", t.Contato);
+                query.Parameters.AddWithValue("@endereco", t.Endereco);
+                query.Parameters.AddWithValue("@id", t.Id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    throw new Exception("Erro ao atualizar o cliente. Verifique e tente novamente.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Delete(Cliente t)
+        {
+            try
+            {
+                var query = conexao.Query();
+                query.CommandText = "delete from Cliente where (clie_id = @id)";
+
+                query.Parameters.AddWithValue("@id", t.Id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    throw new Exception("Erro ao remover o cliente. Verifique e tente novamente.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public Cliente GetById(int id)
+        {
+            try
+            {
+                var query = conexao.Query();
+                query.CommandText = "select * from Cliente where (clie_id = @id)";
+
+                query.Parameters.AddWithValue("@id", id);
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    throw new Exception("Nenhum cliente foi encotrado!");
+                }
+
+                var Cliente = new Cliente();
+
+                while (reader.Read())
+                {
+                    Cliente.Id = reader.GetInt32("clie_id");
+                    Cliente.Nome = AuxiliarDAO.GetString("clie_nome");
+                    Cliente.Sexo = AuxiliarDAO.GetString("clie_sexo");
+                    Cliente.Nascimento = AuxiliarDAO.GetDateTime("clie_nascimento");
+                    Cliente.RG = AuxiliarDAO.GetString("clie_rg");
+                    Cliente.CPF = AuxiliarDAO.GetString("clie_cpf");
+                    Cliente.Email = AuxiliarDAO.GetString("clie_email");
+                    Cliente.Contato = AuxiliarDAO.GetString("clie_contato");
+                    Cliente.Endereco = reader.GetInt32("fk_ende_id");
+                }
+
+                return Cliente;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public List<Cliente> List()
+        {
+            try
+            {
+                List<Cliente> listaCliente = new List<Cliente>();
+
+                var query = conexao.Query();
+                query.CommandText = "select * from Cliente";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    throw new Exception("Nenhum cliente foi encotrado!");
+                }
+
+                while (reader.Read())
+                {
+                    listaCliente.Add(new Cliente()
+                    {
+                        Id = reader.GetInt32("clie_id"),
+                        Nome = AuxiliarDAO.GetString("clie_nome"),
+                        Sexo = AuxiliarDAO.GetString("clie_sexo"),
+                        Nascimento = AuxiliarDAO.GetDateTime("clie_nascimento"),
+                        RG = AuxiliarDAO.GetString("clie_rg"),
+                        CPF = AuxiliarDAO.GetString("clie_cpf"),
+                        Email = AuxiliarDAO.GetString("clie_email"),
+                        Contato = AuxiliarDAO.GetString("clie_contato"),
+                        Endereco = reader.GetInt32("fk_ende_id")
+                    });
+                }
+
+                return listaCliente;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
